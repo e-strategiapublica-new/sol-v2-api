8c0548b25c7d33d87144f8828460468b21636304
Gustavo Guerino <gustavoguerino2@gmail.com>
2024-03-03 19:20:26 -0300

    Add required _id

--- src/modules/SOL/schemas/items.schema.ts
+++ src/modules/SOL/schemas/items.schema.ts
@@ -3,7 +3,7 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 @Schema({ timestamps: true, collection: Items.name.toLowerCase() })
 export class Items {
     
-    @Prop({ type: String })
+    @Prop({ required: true, type: String })
     _id: string; 
 
     @Prop({ required: true, type: String })
