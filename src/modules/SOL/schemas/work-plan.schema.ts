2a1410e8430b863bc444fc8bc2aed03c91de16f9
Gustavo Guerino <gustavoguerino2@gmail.com>
2024-03-03 19:49:33 -0300

    Fix scheme to workplan

--- src/modules/SOL/schemas/work-plan.schema.ts
+++ src/modules/SOL/schemas/work-plan.schema.ts
@@ -15,11 +15,11 @@ export class WorkPlan {
         quantity: { type: Number, required: true },
         unitValue: { type: Number, required: true },
         unit: { type: String },
-        costItems: { type: mongoose.Schema.Types.ObjectId, ref: Items.name, required: true },
+        items: { type: mongoose.Schema.Types.ObjectId, ref: Items.name, required: true },
       },
     ],
   })
-  product: Array<{ quantity: number; unit: string; unitValue: number; costItems: Items }>;
+  product: Array<{ quantity: number; unit: string; unitValue: number; items: Items }>;
 
 }
 export const WorkPlanSchema = SchemaFactory.createForClass(WorkPlan);
